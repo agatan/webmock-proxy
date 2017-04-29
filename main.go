a7ff620afbe3b1dbfffa727a13ff4443c16bbce3
agatan <agatan039@gmail.com>
2017-04-29 15:16:03 +0900

    Move server.go and update main

--- main.go
+++ main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"net/http"
 
-	"github.com/wantedly/webmock-proxy/webmock"
+	"github.com/wantedly/webmock-proxy/cache"
 )
 
 func main() {
-	config, err := webmock.NewConfig()
+	c, err := cache.New(".webmock")
 	if err != nil {
 		log.Fatal(err)
 	}
-	server, err := webmock.NewServer(config)
-	if err != nil {
+	port := flag.Int("port", 8080, "listening port")
+	flag.Parse()
+	var s *server
+	if len(flag.Args()) == 0 || flag.Arg(0) == "replay" {
+		s = newReplayServer(c)
+	} else if flag.Arg(0) == "record" {
+		s = newRecordServer(c)
+	} else {
+		log.Fatalf("no such command: %s", flag.Arg(0))
+	}
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), s); err != nil {
 		log.Fatal(err)
 	}
-	server.Start()
 }
